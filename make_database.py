22f9502c504501055f27df54731957b3df1cd2a9
Tyler Tucker <tylertucker202@yahoo.com>
2017-05-25 10:28:16 -0700

    Add files via upload

--- make_database.py
+++ make_database.py
@@ -9,39 +9,42 @@ Created on Sun Sep 25 09:28:07 2016
 import h5py
 from region_parameters import get_test_tibet_24x24_param
 import pymongo as mongo
-
 import numpy as np
-
+import gzip
 import os
 import logging
 reload(logging)
 import pdb
+import datetime
+from itertools import islice
+from ftplib import FTP
+import urlparse
 
 class make_database:
     
-    def __init__(self, grid_size, db_name, areas_collection):
-        
+    def __init__(self, grid_size, db_name, areas_collection_name, time_series_collection_name):
+
         logging.debug('initializing make_database')
-        self.init_database(db_name)
         self.grid_size = grid_size
-        self.areas_collection = areas_collection
+        self.areas_collection_name = areas_collection_name
         index_array  = np.mgrid[0:self.grid_size, 0:self.grid_size].swapaxes(0,2).swapaxes(0,1)
         self.indicies = index_array.reshape((self.grid_size)*(self.grid_size),2) 
+        self.time_series_collection_name = time_series_collection_name
+        self.init_database(db_name)
         
     def init_database(self,db_name):
         logging.debug('initializing init_database')
         client = mongo.MongoClient('mongodb://localhost:27017/')
         # create database
         self.db = client[db_name]
+        self.areas_collection = self.db[self.areas_collection_name]
 
     def add_area_collection(self, areas_hdf5_name):
         logging.debug('initializing add_area_collection')
         #open hdf5
         fh5 = h5py.File(areas_hdf5_name, "r")
         
-        #create collection and created index
-        collection = self.db[areas_collection]
-        collection.create_index([('lat',mongo.DESCENDING),('long',mongo.DESCENDING)])
+        self.areas_collection.create_index([('lat',mongo.DESCENDING),('long',mongo.DESCENDING)])
         
         #loop over indicies and add area document if index lies on earth.
         for idx, index in enumerate(self.indicies):
@@ -71,7 +74,7 @@ class make_database:
                 }
                 #pdb.set_trace()
                 try:
-                    collection.insert(area_record)
+                    self.areas_collection.insert(area_record)
                 except:
                     logging.warning('not able to add area record: {0} \n row:{1} \n col:{2}'.format(idx, row, col))
                     pass
@@ -81,24 +84,147 @@ class make_database:
                 logging.debug('flushing hdf5')
                 fh5.flush()
                 
-    def add_timeseries_collection(self,output_dir, hdf5_name, collection_name):
+    def get_daily_folder(self,resolution_folder):
+    
+        today = datetime.datetime.now()
+        url = urlparse.urlparse('ftp://sidads.colorado.edu/pub/DATASETS/NOAA/G02156/')
+        IMS_path = 'DATASETS/NOAA/G02156/'
+        
+        year_folder = today.strftime('%Y')
+        
+        prefix = 'ims'
+        postfix = '_'+resolution_folder+'_v1.3.asc.gz'
+        yesterday = today - datetime.timedelta(1)
+        day_filename = yesterday.strftime(prefix+'%Y%j'+postfix)
+        
+        ftp = FTP(url.netloc)    # connect to host, default port
+        ftp.login()                     # user anonymous, passwd anonymous@
+        ftp.cwd(IMS_path)               # change into "debian" directory
+        #ftp.retrlines('LIST')           # list directory contents
+        ftp.cwd(resolution_folder)
+        ftp.cwd(year_folder)
+        
+        try:
+            with open(day_filename, 'w') as fobj:
+                ftp.retrbinary("RETR " + day_filename ,fobj.write)
+        except:
+            print "Error in writing file: {}".format(day_filename)
+        
+        ftp.quit()
+        return day_filename
+
+    def add_today_to_ts_collection(self, resolution, zip_dir):
+        logging.debug('inside add_today_to_ts_collection')
+        logging.debug('retrieving area and time series collections')
+        pdb.set_trace()
+        try:
+            time_series_collection = self.db[self.time_series_collection_name] 
+            #keeps values unique
+            time_series_collection.create_index(
+                [('id_num', mongo.ASCENDING),
+                 ('date', mongo.ASCENDING)],
+                  unique=True)
+        except:
+            logging.warning('not able to get collections')
+            
+        #get today's file
+        today_zip_file_name = self.get_daily_folder(resolution)
+        today = datetime.datetime.today()
+        today_date = today.strftime('%Y-%m-%d')
+        
+        if (not time_series_collection.find_one({'date':today_date}) == None):
+            logging.warning('date: {} already in db. Possible duplicates'.format(today_date))      
+        
+        with gzip.open(today_zip_file_name, 'r') as f:
+            
+            corrupted = False
+            zipped_format = True
+            
+            #loop over rows and add each one to the database
+            start_line = 30
+            for row, line in enumerate(islice(f, start_line, None)):
+    
+                line = line.strip('\n') #sometimes there is a newline
+                line = line.replace('1','0') #remove land and sea to save space
+                line = line.replace('2','0')
+                snow_line = map(lambda x: int(x) ,line)  
+                snow_array = np.array(snow_line)
+                non_zero_elements = np.where(snow_array != 0)
+                
+                if non_zero_elements[0].size == 0:
+                    continue
+                row_of_records = [] #I figured it could be better to insert_many
+                for col, snow_entry in enumerate(snow_line):
+                    
+                    if snow_entry == 0: #empty rows are ignored
+                        continue
+                    area_doc = self.areas_collection.find_one({'row':row, 'col':col})
+                    
+                    if area_doc == None: #areas don't exist along border, so they are removed.
+                        continue
+                    
+                    # add entry to db
+                    snow_record = {
+                    "id_num": area_doc['id_num'],
+                    "snow_data": snow_entry,  
+                    "date": today_date,
+                    "corrupted": bool(corrupted),
+                    "zipped_format": bool(zipped_format),                    
+                    }
+                    
+                    row_of_records.append(snow_record)
+                    
+                    if len(row_of_records) >=500: #keeps row of records small
+                        print('on row: {0}. inserting row of records'.format(row))
+                        time_series_collection.insert_many(row_of_records)
+                        row_of_records = []
+                
+                if row % 250 == 0:
+                    print('on row: {0}'.format(row))
+                    logging.debug('on row: {0}'.format(row))
+                
+                try:                    
+                    if not len(row_of_records)==0:
+                        time_series_collection.insert_many(row_of_records)
+                except:
+                    logging.warning('not able to add row: {0} of today\'s snow record to time series collection'.format(row))
+                    pass     
+                    
+        #check if there is snow 
+        mid_row = time_series_collection.find_one({'row':self.grid_size/2, 'date':today_date, 'snow_data': { '$in': [3, 4] } })
+        if mid_row == None:
+            logging.warning('no snow or ice displayed on middle row...setting as corrupt')
+            logging.warning('filename: {}'.format(today_zip_file_name))
+            time_series_collection.update_many({'date':today_date}, {'corrupted':bool(True)})
+                        
+        
+        #move file to zip_file directory 
+        
+        logging.debug('move {0} to zip_file directory: {1}'.format(today_zip_file_name,zip_dir))
+        os.rename(today_zip_file_name, os.path.join(zip_dir,today_zip_file_name))
+        logging.debug('leaving add_today_to_ts_collection')
+        
+                
+    def add_timeseries_collection(self,output_dir, hdf5_name):
 
         logging.debug('initializing add_timeseries_collection')
         
         logging.debug('retrieving area collection')
         try:
-            area_collection = self.db[self.areas_collection]
-            curser = area_collection.find()
+            curser = self.area_collection.find()
             id_list = []
             for area_doc in curser[:]: #can only query once. need to get ID number
                 id_list.append(area_doc['id_num'])
         except:
             logging.warning('not able to retrieve area collection')
     
-        logging.debug('creating time series collection: {0}'.format(collection_name))
+        logging.debug('creating time series collection: {0}'.format(self.collection_name))
         try:
-            collection = self.db[collection_name]
-            collection.create_index([('lat',mongo.DESCENDING),('long',mongo.DESCENDING), ('date',mongo.DESCENDING)])
+            time_series_collection = self.db[self.time_series_collection_name]
+            time_series_collection.create_index([('lat',mongo.DESCENDING),
+                                                 ('long',mongo.DESCENDING),
+                                                 ('date',mongo.DESCENDING)],
+                                                  unique=True)
         except:
             logging.warning('not able to create time series collection')
 
@@ -116,8 +242,8 @@ class make_database:
                 
                 for id_elem in id_list: #loop over areas in database
                     pdb.set_trace()
-                    if (not collection.find_one({'id_num':id_elem}) == None) and (not date == ""):
-                        area_doc = area_collection.find_one({'id_num':id_elem})
+                    if (not time_series_collection.find_one({'id_num':id_elem}) == None) and (not date == ""):
+                        area_doc = self.area_collection.find_one({'id_num':id_elem})
     
                         row = area_doc['row']
                         col = area_doc['col']
@@ -132,7 +258,7 @@ class make_database:
                         }
                         
                         try:
-                            collection.insert(snow_record)
+                            time_series_collection.insert(snow_record)
                         except:
                             logging.warning('not able to add snow record to time series collection')
             
@@ -163,11 +289,18 @@ if __name__ == '__main__':
     #init database
     db_name = 'test-database-2'
     areas_collection = 'twenty_four_km_areas'
-    collection_name = 'twenty_four_km_time_series'
+    collection_name = 'test_twenty_four_km_time_series'
     hdf5_name = 'combined.hdf5'
+    resolution = '24km'
+    zip_dir = os.path.join(home_dir,os.pardir,'zip_files',resolution)
     
-    mdb = make_database(grid_size, db_name, areas_collection)
-    mdb.add_timeseries_collection(output_dir, hdf5_name, collection_name)
+    if not os.path.exists(zip_dir):
+        logging.debug('making directory: {0}'.format(zip_dir))
+        os.mkdir(zip_dir)
+    
+    mdb = make_database(grid_size, db_name, areas_collection,collection_name)
+    mdb.add_today_to_ts_collection(resolution, zip_dir)
+    #mdb.add_timeseries_collection(output_dir, hdf5_name)
 
 
         
